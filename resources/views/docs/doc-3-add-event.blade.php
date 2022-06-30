56f20b46674684209bd53c66a424be19f6ee72bf
ptrakhsa <putriaqsadini@gmail.com>
2022-06-30 18:17:08 +0800

    update; doc

--- resources/views/docs/doc-3-add-event.blade.php
+++ resources/views/docs/doc-3-add-event.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.docs-panel')
 @section('content')
-    <h3>Validate event</h3>
-    <h3>Add event</h3>
-    <h4>Reject event</h4>
-    <h4>Verify event</h4>
-    <h4>Takedown event</h4>
-    <h4>Mark event as done</h4>
+<h3>Validate event</h3>
+<pre>set @start = '2022-06-30 06:45:46', @end = '2022-07-03 06:45:46', @pos = GEOMETRYFROMTEXT('POINT(110.36588792192126 -7.791903826254844)', 4326);
+
+SELECT events.id, ST_DISTANCE_SPHERE(events.position, @pos, 4326)
+as distance, events.name , (SELECT submitted_events.status 
+                            FROM submitted_events  WHERE events.id = submitted_events.event_id ORDER BY id DESC LIMIT 1) as last_status 
+FROM events 
+WHERE ST_DISTANCE_SPHERE(events.position, @pos, 4326) <= 0.5 AND (SELECT submitted_events.status FROM submitted_events WHERE events.id = submitted_events.event_id ORDER BY id DESC LIMIT 1) = 'verified' AND (events.start_date between @start and @end
+OR events.end_date between @start and @end
+OR (@start between events.start_date and events.end_date)
+);</pre>
+<h3>Add event</h3>
+<h4>Reject event</h4>
+<h4>Verify event</h4>
+<h4>Takedown event</h4>
+<h4>Mark event as done</h4>
 @endsection
---
+++ resources/views/docs/doc-4-uwiii.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.docs-panel')
+@section('content')
+<h3>Architecture</h3>
+<img src="/assets/images/contents/architecture.png" class="img-fluid" />
+hey
+@endsection
