bc22d0c856937afc627d379cbe38981b015f045e
ajisyahroni <aji.0508@students.amikom.ac.id>
2022-07-08 10:13:15 +0700

    change : description string

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -31,7 +31,7 @@
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
                     <h3>Dashboard</h3>
-                    <p class="text-subtitle text-muted">A bunch of unapproved incoming events</p>
+                    <p class="text-subtitle text-muted">Incoming and upcoming events</p>
                     <div class="mb-5 d-flex">
 
                         <a href="/admin/dashboard?status=incoming"
