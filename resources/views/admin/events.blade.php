6751b2d925d82f67b207f2a81180f672c3c50906
ajisyahroni <aji.0508@students.amikom.ac.id>
2022-07-08 10:25:59 +0700

    update : event page description string

--- resources/views/admin/events.blade.php
+++ resources/views/admin/events.blade.php
@@ -10,7 +10,7 @@
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
                     <h3>Events</h3>
-                    <p class="text-subtitle text-muted">List of events group by verified and rejected</p>
+                    <p class="text-subtitle text-muted">List of events group by event status</p>
                 </div>
             </div>
         </div>
