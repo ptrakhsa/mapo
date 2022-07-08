e13aac4df4b2a95e7dc8ca7eb8e701371dc06145
ajisyahroni <aji.0508@students.amikom.ac.id>
2022-07-08 10:28:16 +0700

    change : update string description

--- resources/views/admin/place-boundaries.blade.php
+++ resources/views/admin/place-boundaries.blade.php
@@ -40,7 +40,7 @@
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
                     <h3>Place boundaries</h3>
-                    <p class="text-subtitle text-muted">count events each place boundaries</p>
+                    <p class="text-subtitle text-muted">count events by place boundaries</p>
                 </div>
             </div>
         </div>
