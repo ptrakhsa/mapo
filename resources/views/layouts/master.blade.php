378f8cae8a8c3ad90256bd95763cd53ad439d3ae
ptrakhsa <putriaqsadini@gmail.com>
2022-07-19 00:25:07 +0800

    add: logo.svg

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,8 +15,7 @@
     <link rel="stylesheet" href="/assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
     <link rel="stylesheet" href="/assets/vendors/bootstrap-icons/bootstrap-icons.css">
     <link rel="stylesheet" href="/assets/css/app.css">
-    <link rel="shortcut icon" href="/assets/images/logo/logo.png" type="image/x-icon">
-    <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/images/logo/logo.svg">
 
     <link rel="stylesheet" href="/assets/vendors/fontawesome/all.min.css">
     <link rel="stylesheet" href="/assets/vendors/bootstrap-icons/bootstrap-icons.css">
